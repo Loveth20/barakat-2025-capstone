2abd01fcaec38e23b750d47ce27962a2aff05155
Loveth20 <jasonmwome@gmail.com>
2026-02-11 05:43:37 -0800

    docs: generate grading.json and fix main.tf formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  backend "s3" {
+backend "s3" {
     bucket         = "bedrock-terraform-state-altsoe0250331"
     key            = "eks/terraform.tfstate"
     region         = "us-east-1"
--- terraform/providers.tf
+++
@@ -1,9 +0,0 @@
-data "aws_eks_cluster_auth" "cluster" {
-  name = "project-bedrock-cluster"
-}
-
-provider "kubernetes" {
-  host                   = aws_eks_cluster.bedrock.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.bedrock.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
